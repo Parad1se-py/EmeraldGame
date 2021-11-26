ef4b2bdf3a02135241c77867cd37f9dc3559d80e
Arjun Ladha <tigerninja2234@gmail.com>
2021-11-26 09:30:55 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Emerald',
+    version='0.1.0',
+    description='A fun 2d RPG game',
+    long_description=readme,
+    author='Team Frixion',
+    author_email='ajcodes22@gmail.com',
+    url='https://github.com/',
+    license=license,
+)
